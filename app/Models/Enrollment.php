74bedcc47f2e9572b0f15af7998e24b47ffc2e09
tirtahakimpambudhi <awebthp@gmail.com>
2023-11-06 19:07:15 +0700

    chore: Add Enrollment.php files

---
+++ app/Models/Enrollment.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Status;
+use Database\Factories\EnrollmentFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Enrollment extends Model
+{
+    /** @use HasFactory<EnrollmentFactory> */
+    use HasFactory;
+
+    protected $table = 'enrollments';
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'course_id',
+        'student_id',
+        'enrolled_at',
+    ];
+
+    protected static function boot() :void
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::ulid();
+            }
+        });
+    }
+
+    protected $casts = [
+        'status' => Status::class,
+    ];
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+}
