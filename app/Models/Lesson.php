c9c311d4dcb60fc977669e2f4acc12e790555ec8
tirtahakimpambudhi <awebthp@gmail.com>
2023-11-07 19:07:16 +0700

    chore: Add Lesson.php files

---
+++ app/Models/Lesson.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Lesson extends Model
+{
+    /** @use HasFactory<\Database\Factories\LessonFactory> */
+    use HasFactory;
+
+    protected $table = 'lessons';
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+      'title',
+      'description',
+      'video_link',
+        'duration',
+        'order_number',
+        'course_id',
+    ];
+
+    public function course() : BelongsTo {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+
+    protected static function boot() :void
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::ulid();
+            }
+        });
+    }
+}
