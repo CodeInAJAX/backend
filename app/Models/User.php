7f0ece28e8fab60eac98aa1f7d7d85f57bb0aaaf
tirtahakimpambudhi <awebthp@gmail.com>
2023-11-10 19:07:17 +0700

    chore: Add User.php files

---
+++ app/Models/User.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\Role;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    use SoftDeletes;
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'role',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'role' => Role::class,
+        ];
+    }
+
+    public function courses () : HasMany
+    {
+        return $this->hasMany(Course::class, 'mentor_id');
+    }
+
+    public function profile () : HasOne
+    {
+        return $this->hasOne(Profile::class, 'user_id');
+    }
+
+    public function enrolledCourses() : BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'enrollments', 'student_id', 'course_id')
+            ->withTimestamps()
+            ->withPivot( 'status');
+    }
+
+    public function ratingsCourses() : BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'ratings', 'student_id', 'course_id')
+            ->withTimestamps()
+            ->withPivot('rating', 'comment');
+    }
+
+    protected static function boot() :void
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::ulid();
+            }
+        });
+    }
+
+}
