6f91de4c1dce4387d4d77e28dad3bfaf7a66dae5
tirtahakimpambudhi <awebthp@gmail.com>
2023-12-13 19:07:30 +0700

    chore: Add 2025_05_05_043435_create_lessons_table.php files

---
+++ database/migrations/2025_05_05_043435_create_lessons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('title')->nullable();
+            $table->text('description');
+            $table->string('video_link')->nullable();
+            $table->integer('duration')->nullable();
+            $table->integer('order_number')->nullable();
+            $table->foreignUlid('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
