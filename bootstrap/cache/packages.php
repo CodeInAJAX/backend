de9d2302741770360f8132dab583aa7757aae867
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-09 17:24:49 +0700

    chore(package): :package: Update packages.php file for manage dependencies library

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -66,4 +66,16 @@
       0 => 'Scalar\\ScalarServiceProvider',
     ),
   ),
+  'tymon/jwt-auth' => 
+  array (
+    'aliases' => 
+    array (
+      'JWTAuth' => 'Tymon\\JWTAuth\\Facades\\JWTAuth',
+      'JWTFactory' => 'Tymon\\JWTAuth\\Facades\\JWTFactory',
+    ),
+    'providers' => 
+    array (
+      0 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
+    ),
+  ),
 );
