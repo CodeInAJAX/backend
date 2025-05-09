827f05b4a71facae676ce2095d4c8a855e750fff
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-10 05:15:21 +0700

    chore: Add LoginUserRequest.php files

---
+++ app/Http/Requests/LoginUserRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class LoginUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                Rule::email()
+                    ->rfcCompliant(strict: false)
+                    ->validateMxRecord()
+                    ->preventSpoofing(),
+                'exists:users,email',
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+            ],
+            'confirm_password' => [
+                'required',
+                'string',
+                'min:8',
+                'same:password',
+            ]
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorValidatorToResponse($validator)
+        );
+    }
+}
