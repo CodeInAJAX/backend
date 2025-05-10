c3294ef3b70f2e0b97d51ef5ab917c73787fff65
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-10 20:28:13 +0700

    chore: Add FilterUserByEmailRequest.php files

---
+++ app/Http/Requests/FilterUserByEmailRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\HttpResponses;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class FilterUserByEmailRequest extends FormRequest
+{
+    use HttpResponses;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                Rule::email()
+                    ->rfcCompliant(strict: false)
+                    ->validateMxRecord()
+                    ->preventSpoofing(),
+                'exists:users,email',
+            ]
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorValidatorToResponse($validator)
+        );
+    }
+
+    public function validationData(): array
+    {
+        return [
+          'email' => $this->route('email'),
+        ];
+    }
+}
