a4354dd52e810c9339bb55216f371738116f86b6
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-11 05:15:26 +0700

    chore: Update UserService.php files

--- app/Service/Contracts/UserService.php
+++ app/Service/Contracts/UserService.php
@@ -2,15 +2,22 @@
 
 namespace App\Service\Contracts;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
+use Illuminate\Cookie\Cookie;
+
 
 interface UserService
 {
     public function getAll() :UserResource;
 
     public function create(StoreUserRequest $data) : UserResource;
+    public function login(LoginUserRequest $data) : array;
+
+    public function setCookieWithRefreshToken(string $refreshToken) :Cookie;
+
     public function update(string $id, UpdateUserRequest $data) : UserResource;
     public function delete(string $id) : bool;
 
