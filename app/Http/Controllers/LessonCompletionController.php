73f192a0febc7ecad44624a7b902567713434731
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-11 21:15:51 +0700

    chore: Add LessonCompletionController.php files

---
+++ app/Http/Controllers/LessonCompletionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LessonCompletion;
+use App\Http\Requests\StoreLessonCompletionRequest;
+use App\Http\Requests\UpdateLessonCompletionRequest;
+
+class LessonCompletionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLessonCompletionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LessonCompletion $lessonCompletion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LessonCompletion $lessonCompletion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLessonCompletionRequest $request, LessonCompletion $lessonCompletion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LessonCompletion $lessonCompletion)
+    {
+        //
+    }
+}
