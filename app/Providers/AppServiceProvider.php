429e20cb37ba3377669b80411cd6efce1880c4b9
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-13 20:28:15 +0700

    chore: Update AppServiceProvider.php files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\Authenticate;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,31 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::automaticallyEagerLoadRelationships();
+        Authenticate::redirectUsing(function ($request) {
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'errors' => [
+                        'title' => 'Users Unauthorized',
+                        'detail' => 'You must authenticate to perform this action.',
+                        'code' => Response::HTTP_UNAUTHORIZED,
+                        'status' => 'STATUS_UNAUTHORIZED',
+                    ]
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'errors' => [
+                        'title' => 'Users Unauthorized',
+                        'detail' => 'You must authenticate to perform this action.',
+                        'code' => Response::HTTP_UNAUTHORIZED,
+                        'status' => 'STATUS_UNAUTHORIZED',
+                    ]
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+            return redirect()->guest(route('login'));
+        });
     }
 }
