368857e85122a5a284dbd6744407b10a70d3401d
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-14 21:15:53 +0700

    chore: Update StoreCourseRequest.php files

--- app/Http/Requests/StoreCourseRequest.php
+++ app/Http/Requests/StoreCourseRequest.php
@@ -2,27 +2,66 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\HttpResponses;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreCourseRequest extends FormRequest
 {
+    use HttpResponses;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorValidatorToResponse($validator)
+        );
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'title' => [
+                'required',
+                'string',
+                'min:3',
+                'max:255',
+            ],
+            'thumbnail' => [
+                'required',
+                'string',
+                'url:http,https',
+            ],
+            'description' => [
+                'required',
+                'string'
+            ],
+            'price' => [
+                'required',
+                'integer',
+            ],
+            'currency' => [
+                'required',
+                'string',
+            ]
         ];
+    }
+
+    public function validationData(): array
+    {
+        return $this->only('title','thumbnail', 'description', 'price', 'currency');
     }
 }
