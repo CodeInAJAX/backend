43da8c7bf114befd1d8d5b0d8a756d22cd484300
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-14 20:28:16 +0700

    chore: Update UserService.php files

--- app/Service/Contracts/UserService.php
+++ app/Service/Contracts/UserService.php
@@ -2,11 +2,12 @@
 
 namespace App\Service\Contracts;
 
+use App\Http\Requests\FilterUserByEmailRequest;
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
-use Illuminate\Cookie\Cookie;
+use \Symfony\Component\HttpFoundation\Cookie;
 
 
 interface UserService
@@ -17,6 +18,8 @@ interface UserService
     public function login(LoginUserRequest $data) : array;
 
     public function setCookieWithRefreshToken(string $refreshToken) :Cookie;
+
+    public function getByEmail(FilterUserByEmailRequest $data) : UserResource;
 
     public function update(string $id, UpdateUserRequest $data) : UserResource;
     public function delete(string $id) : bool;
