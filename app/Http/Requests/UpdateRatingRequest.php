61438da599ec8ca90914e9b6055f7a348a3600ca
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-15 14:20:28 +0700

    chore: Update UpdateRatingRequest.php files

--- app/Http/Requests/UpdateRatingRequest.php
+++ app/Http/Requests/UpdateRatingRequest.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\HttpResponses;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UpdateRatingRequest extends FormRequest
 {
+    use HttpResponses;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +26,41 @@ class UpdateRatingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'rating' => [
+                'sometimes',
+                'integer',
+                'min:1',
+                'max:5',
+            ],
+            'comment' => [
+                'sometimes',
+                'string',
+            ]
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'rating.integer' => 'Rating harus berupa angka.',
+            'rating.min' => 'Rating minimal adalah 1.',
+            'rating.max' => 'Rating maksimal adalah 5.',
+
+            'comment.string' => 'Komentar harus berupa teks.',
+        ];
+    }
+
+
+    public function validationData(): array
+    {
+        return $this->only('rating', 'comment');
+    }
+
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorValidatorToResponse($validator)
+        );
     }
 }
