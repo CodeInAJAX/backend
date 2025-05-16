0ea7c9a7265b032a73fa72ca457454a80e352e0a
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-16 21:15:55 +0700

    chore: Update StoreLessonRequest.php files

--- app/Http/Requests/StoreLessonRequest.php
+++ app/Http/Requests/StoreLessonRequest.php
@@ -2,27 +2,66 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\HttpResponses;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class StoreLessonRequest extends FormRequest
 {
+    use HttpResponses;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorValidatorToResponse($validator)
+        );
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'title' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'description' => [
+                'required',
+                'string'
+            ],
+            'video_link' => [
+                'required',
+                'string',
+                'url:http,https'
+            ],
+            'duration' => [
+                'required',
+                'integer',
+            ],
+            'order_number' => [
+                'required',
+                'integer'
+            ]
         ];
+    }
+
+    public function validationData(): array
+    {
+        return $this->only('title', 'description', 'video_link', 'duration', 'order_number');
     }
 }
