e041663e339be3805006a0070b968c86fb61618a
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-22 21:30:44 +0700

    chore: Update StoreEnrollmentRequest.php files

--- app/Http/Requests/StoreEnrollmentRequest.php
+++ app/Http/Requests/StoreEnrollmentRequest.php
@@ -2,27 +2,59 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\Status;
+use App\Traits\HttpResponses;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class StoreEnrollmentRequest extends FormRequest
 {
+    use HttpResponses;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'course_id' => [
+                'required',
+                'string',
+                'exists:courses,id',
+            ]
         ];
+    }
+
+    public function messages(): array {
+        return [
+            'course_id.required' => 'ID Kursus harus diisi',
+            'course_id.string' => 'ID Kursus tidak valid',
+            'course_id.exists' => 'ID Kursus tidak ditemukan',
+
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorValidatorToResponse($validator)
+        );
+    }
+
+    public function validationData() :array
+    {
+        return $this->only('course_id');
     }
 }
