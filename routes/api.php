f5fffcf33be08146bcf4d9c915ccad289e263812
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-28 09:32:02 +0700

    chore: Update api.php files

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 $this->router->name('api.users.')->prefix('/v1/users')->controller(UserController::class)->group(function () {
+    $this->router->name('index')->get('/', 'index');
+    $this->router->name('refresh')->get('/refresh-token', 'refresh');
     $this->router->name('create')->post('/', 'store');
     $this->router->name('login')->post('/login', 'login');
     $this->router->name('logout')->delete('/logout', 'logout');
     $this->router->name('show')->get('/{email}', 'show');
+    $this->router->name('update')->patch('/', 'update');
+    $this->router->name('destroy')->delete('/{id}', 'destroy');
+    $this->router->name('restore')->post('/restore/{id}', 'restore');
 });
