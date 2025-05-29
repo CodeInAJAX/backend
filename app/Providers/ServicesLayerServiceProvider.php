530dfeac7794e8ee72d2f0b36dd6e47e27347c9e
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-29 21:16:02 +0700

    chore: Update ServicesLayerServiceProvider.php files

--- app/Providers/ServicesLayerServiceProvider.php
+++ app/Providers/ServicesLayerServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\UserController;
+use App\Models\Course;
 use App\Models\User;
+use App\Service\Contracts\CourseService;
 use App\Service\Contracts\UserService;
+use App\Service\Implements\CourseServiceImpl;
 use App\Service\Implements\UserServiceImpl;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Log\Logger;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Contracts\Auth\Factory as AuthFactory;
+use Illuminate\Contracts\Auth\Access\Gate;
 
 class ServicesLayerServiceProvider extends ServiceProvider implements DeferrableProvider
 {
@@ -20,17 +25,39 @@ class ServicesLayerServiceProvider extends ServiceProvider implements Deferrable
         $this->app->bind(User::class, function ($app) {
             return new User();
         });
+
+        $this->app->bind(Course::class, function ($app) {
+            return new Course();
+        });
+
         $this->app->bind(
             UserService::class,
             function ($app) {
-                $userModel = $app->make(User::class);
-                $logger = $app->make(Logger::class);
                 return new UserServiceImpl(
-                    $userModel,
-                    $logger
+                    $app->make(User::class),
+                    $app->make(Logger::class),
+                    $app->make(AuthFactory::class),
+                    $app->make(Gate::class)
                 );
             }
         );
+
+        $this->app->bind(CourseService::class, function ($app) {
+            return new CourseServiceImpl(
+                $app->make(Course::class),
+                $app->make(Logger::class),
+                $app->make(AuthFactory::class),
+                $app->make(Gate::class)
+            );
+        });
+
+        $this->app->bind(UserController::class, function ($app) {
+            return new UserController(
+                $app->make(UserService::class),
+                $app->make(Logger::class),
+            );
+        });
+
     }
 
     /**
@@ -42,6 +69,6 @@ class ServicesLayerServiceProvider extends ServiceProvider implements Deferrable
     }
 
     public function provides(): array {
-        return [UserService::class];
+        return [UserService::class, UserController::class, CourseService::class];
     }
 }
