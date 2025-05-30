b5637d68be8c2a82dd5c9664156c0f491a747da9
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-30 21:16:02 +0700

    chore: Add CourseService.php files

---
+++ app/Service/Contracts/CourseService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service\Contracts;
+
+use App\Http\Requests\PaginationRequest;
+use App\Http\Requests\StoreCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
+use App\Http\Resources\CourseResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+interface CourseService
+{
+    public function getAll(PaginationRequest $data) : AnonymousResourceCollection;
+
+    public function show(string $id) : CourseResource;
+
+    public function create(StoreCourseRequest $data) : CourseResource;
+
+    public function update(string $id,UpdateCourseRequest $data) : CourseResource;
+
+    public function delete(string $id) : array;
+}
