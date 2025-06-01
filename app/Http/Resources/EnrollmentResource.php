16be9c733e0a0cba74216a8ac28e5de14d8cc571
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-01 21:30:47 +0700

    chore: Add EnrollmentResource.php files

---
+++ app/Http/Resources/EnrollmentResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EnrollmentResource extends JsonResource
+{
+    public static function collection($resource): AnonymousResourceCollection
+    {
+        return parent::collection($resource);
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'courseId' => $this->id,
+            'studentId' => $this->student_id,
+            'status' => $this->status,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'course' => new CourseResource($this->whenLoaded('course')),
+            'student' => new UserResource($this->whenLoaded('student')),
+        ];
+    }
+}
