58fd56a550c50be054e706359c9df0fc9bc1c5b8
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-06 19:24:10 +0700

    chore: Update api.php files

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+$this->router->name('api.users.')->prefix('/v1/users')->controller(UserController::class)->group(function () {
+    $this->router->name('create')->post('/', 'store');
+});
