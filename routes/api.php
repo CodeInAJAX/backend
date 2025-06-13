d7eba2500ad11320f55ca5712ebb5cf1952ba446
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-13 21:16:12 +0700

    chore: Update api.php files

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 $this->router->name('api.users.')->prefix('/v1/users')->controller(UserController::class)->group(function () {
     $this->router->name('index')->get('/', 'index');
+    $this->router->name('trash')->get('/trash', 'trashAll');
+    $this->router->name('showTrash')->post('/trash/{id}', 'showTrash');
     $this->router->name('refresh')->get('/refresh-token', 'refresh');
     $this->router->name('create')->post('/', 'store');
     $this->router->name('login')->post('/login', 'login');
@@ -18,4 +16,12 @@ $this->router->name('api.users.')->prefix('/v1/users')->controller(UserControlle
     $this->router->name('update')->patch('/', 'update');
     $this->router->name('destroy')->delete('/{id}', 'destroy');
     $this->router->name('restore')->post('/restore/{id}', 'restore');
+});
+
+$this->router->name('api.courses.')->prefix('/v1/courses')->controller(CourseController::class)->group(function () {
+    $this->router->name('index')->get('/', 'index');
+    $this->router->name('create')->post('/', 'store');
+    $this->router->name('show')->get('/{id}', 'show');
+    $this->router->name('update')->patch('/{id}', 'update');
+    $this->router->name('destroy')->delete('/{id}', 'destroy');
 });
