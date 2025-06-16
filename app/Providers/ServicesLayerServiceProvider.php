5aae45841ae9bbb4bf9f7fcce3716ed30e797487
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-16 21:30:54 +0700

    chore: Update ServicesLayerServiceProvider.php files

--- app/Providers/ServicesLayerServiceProvider.php
+++ app/Providers/ServicesLayerServiceProvider.php
@@ -4,10 +4,19 @@ namespace App\Providers;
 
 use App\Http\Controllers\UserController;
 use App\Models\Course;
+use App\Models\Enrollment;
+use App\Models\Lesson;
+use App\Models\Payment;
 use App\Models\User;
 use App\Service\Contracts\CourseService;
+use App\Service\Contracts\EnrollmentService;
+use App\Service\Contracts\LessonService;
+use App\Service\Contracts\PaymentService;
 use App\Service\Contracts\UserService;
 use App\Service\Implements\CourseServiceImpl;
+use App\Service\Implements\EnrollmentServiceImpl;
+use App\Service\Implements\LessonServiceImpl;
+use App\Service\Implements\PaymentServiceImpl;
 use App\Service\Implements\UserServiceImpl;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Log\Logger;
@@ -30,6 +39,18 @@ class ServicesLayerServiceProvider extends ServiceProvider implements Deferrable
             return new Course();
         });
 
+        $this->app->bind(Lesson::class, function ($app) {
+            return new Lesson();
+        });
+
+        $this->app->bind(Payment::class, function ($app) {
+           return new Payment();
+        });
+
+        $this->app->bind(Enrollment::class, function ($app) {
+            return new Enrollment();
+        });
+
         $this->app->bind(
             UserService::class,
             function ($app) {
@@ -45,6 +66,38 @@ class ServicesLayerServiceProvider extends ServiceProvider implements Deferrable
         $this->app->bind(CourseService::class, function ($app) {
             return new CourseServiceImpl(
                 $app->make(Course::class),
+                $app->make(Logger::class),
+                $app->make(AuthFactory::class),
+                $app->make(Gate::class)
+            );
+        });
+
+        $this->app->bind(LessonService::class, function ($app) {
+            return new LessonServiceImpl(
+                $app->make(Lesson::class),
+                $app->make(Course::class),
+                $app->make(Logger::class),
+                $app->make(AuthFactory::class),
+                $app->make(Gate::class)
+            );
+        });
+
+        $this->app->bind(PaymentService::class, function ($app) {
+            return new PaymentServiceImpl(
+                $app->make(Payment::class),
+                $app->make(Course::class),
+                $app->make(Enrollment::class),
+                $app->make(Logger::class),
+                $app->make(AuthFactory::class),
+                $app->make(Gate::class)
+            );
+        });
+
+        $this->app->bind(EnrollmentService::class, function ($app) {
+            return new EnrollmentServiceImpl(
+                $app->make(Payment::class),
+                $app->make(Course::class),
+                $app->make(Enrollment::class),
                 $app->make(Logger::class),
                 $app->make(AuthFactory::class),
                 $app->make(Gate::class)
@@ -69,6 +122,6 @@ class ServicesLayerServiceProvider extends ServiceProvider implements Deferrable
     }
 
     public function provides(): array {
-        return [UserService::class, UserController::class, CourseService::class];
+        return [UserService::class, UserController::class, CourseService::class,  LessonService::class, PaymentService::class, EnrollmentService::class];
     }
 }
